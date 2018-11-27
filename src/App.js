6ccd0e268a2f46c71188945dd0cb08a22651a55b
Waldemar Penner <Waldemar.Penner91@googlemail.com>
2018-11-27 14:10:24 +0100

    Add styles

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Map from "pigeon-maps";
 import Marker from "pigeon-marker";
 import capitalCities from "./capitalCities";
 import { getDistance, convertUnit } from "geolib";
+import Button from "./components/button/Button";
+import { css } from "emotion";
 
 class App extends Component {
   defaultCenter = [50.520008, 10];
@@ -44,21 +46,62 @@ class App extends Component {
     });
   };
   render() {
+    const descriptionStyle = css`
+      font-weight: 500;
+      letter-spacing: 1px;
+      text-transform: uppercase;
+      font-size: 1.4rem;
+      opacity: 0.75;
+    `;
     return (
-      <>
-        <span>{this.state.kilometersLeft} kilometers left</span>
-        {this.state.distance ? (
-          <span>
-            The distance between your marker and "
-            {capitalCities[this.state.currentCityIndex].capitalCity}" is{" "}
-            {this.state.distance} kilometers
-          </span>
-        ) : (
-          <span>
-            Select the location of "
-            {capitalCities[this.state.currentCityIndex].capitalCity}"
-          </span>
-        )}
+      <div
+        className={css`
+          display: flex;
+          flex-direction: column;
+          width: 80rem;
+          margin-top: 2rem;
+          border-radius: 10px;
+          box-shadow: 0 2px 6px 0 hsl(247, 52%, 12%, 0.2);
+          overflow: hidden;
+          background-color: #fff;
+        `}
+      >
+        <header
+          className={css`
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            min-height: 7rem;
+            padding: 2rem 1rem;
+            align-items: center;
+          `}
+        >
+          <h1
+            className={css`
+              font-weight: 700;
+              font-style: italic;
+              font-size: 2.5rem;
+              margin-bottom: 2rem;
+              text-align: center;
+            `}
+          >
+            {this.state.score} {this.state.score === 1 ? "city " : "cities "}
+            placed <br />
+            {this.state.kilometersLeft} kilometers left
+          </h1>
+          {this.state.distance ? (
+            <span className={descriptionStyle}>
+              The distance between your marker and "
+              {capitalCities[this.state.currentCityIndex].capitalCity}" is{" "}
+              {this.state.distance} kilometers
+            </span>
+          ) : (
+            <span className={descriptionStyle}>
+              Select the location of "
+              {capitalCities[this.state.currentCityIndex].capitalCity}"
+            </span>
+          )}
+        </header>
         <Map
           provider={(x, y, z) =>
             `https://api.mapbox.com/styles/v1/w0wka91/cjozn6qj6kwhp2rlm2m8xtoa7/tiles/256/${z}/${x}/${y}?access_token=pk.eyJ1IjoidzB3a2E5MSIsImEiOiJjam96bDZ3ZWgwOHV5M3FwZjQ5Z29kY2w5In0.COtOH3pcEaqshmLryd8yAg#12.0/48.866500/2.317600/0`
@@ -67,7 +110,7 @@ class App extends Component {
           onClick={({ latLng }) => this.setState({ markerPosition: latLng })}
           zoom={this.state.zoom}
           width={800}
-          height={600}
+          height={500}
         >
           {this.state.markerPosition && (
             <Marker anchor={this.state.markerPosition} />
@@ -82,15 +125,28 @@ class App extends Component {
             />
           )}
         </Map>
-        <button
-          onClick={
-            this.state.distance ? this.calcRemainingKm : this.calcDistance
-          }
-          disabled={this.state.markerPosition ? false : true}
+        <footer
+          className={css`
+            display: flex;
+            flex-direction: row-reverse;
+            height: 7rem;
+            padding: 2rem 1rem;
+            align-items: center;
+            > * {
+              margin-left: 1rem;
+            }
+          `}
         >
-          {this.state.distance ? "Next" : "Confirm"}
-        </button>
-      </>
+          <Button
+            onClick={
+              this.state.distance ? this.calcRemainingKm : this.calcDistance
+            }
+            disabled={this.state.markerPosition ? false : true}
+          >
+            {this.state.distance ? "Next" : "Confirm"}
+          </Button>
+        </footer>
+      </div>
     );
   }
 }
